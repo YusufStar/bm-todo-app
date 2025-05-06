6380e991b6c93a8cfaa3f85fc1d16fd15d0a6bbe
Yusuf yıldız <me@yusufstar.com>
2025-05-06 21:54:45 +0300

    models created

--- server/src/database/models/user.model.ts
+++ server/src/database/models/user.model.ts
@@ -1,43 +1,75 @@
-import { model, Schema } from "mongoose"
+import mongoose, { Document, Schema } from "mongoose";
+import { compareValue, hashValue } from "../../common/utils/bcrypt";
 
 interface UserPreferences {
-    enable2FA: boolean
-    emailNotification: boolean
-    twoFactorSecret?: string
+    enable2FA: boolean;
+    emailNotification: boolean;
+    twoFactorSecret?: string;
 }
 
 export interface UserDocument extends Document {
-    name: string
-    email: string
-    password: string
-    isEmailVerified: boolean
-    createdAt: Date
-    updatedAt: Date
-    userPreferences: UserPreferences
-    comparePassword: (candidatePassword: string) => Promise<boolean>
+    name: string;
+    email: string;
+    password: string;
+    isEmailVerified: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    userPreferences: UserPreferences;
+    comparePassword(value: string): Promise<boolean>;
 }
-
-const userPreferences = new Schema<UserPreferences>({
+const userPreferencesSchema = new Schema<UserPreferences>({
     enable2FA: { type: Boolean, default: false },
     emailNotification: { type: Boolean, default: true },
-    twoFactorSecret: { type: String, default: false },
-})
+    twoFactorSecret: { type: String, required: false },
+});
 
-const userSchema = new Schema<UserDocument>({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isEmailVerified: { type: Boolean, default: false },
-    userPreferences: { type: userPreferences, default: {} },
-}, { timestamps: true, toJSON: {} })
+const userSchema = new Schema<UserDocument>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+        userPreferences: {
+            type: userPreferencesSchema,
+            default: {},
+        },
+    },
+    {
+        timestamps: true,
+        toJSON: {},
+    }
+);
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await hashValue(this.password);
+    }
+    next();
+});
+
+userSchema.methods.comparePassword = async function (value: string) {
+    return compareValue(value, this.password);
+};
 
 userSchema.set("toJSON", {
     transform: function (doc, ret) {
-        delete ret.password
-        delete ret.userPreferences.twoFactorSecret
-        return ret
-    }
-})
+        delete ret.password;
+        delete ret.userPreferences.twoFactorSecret;
+        return ret;
+    },
+});
 
-const UserModel = model<UserDocument>("User", userSchema)
-export default UserModel
+const UserModel = mongoose.model<UserDocument>("User", userSchema);
+export default UserModel;
