2c99e52294b791b666d3ff56820e505457362d06
Yusuf yıldız <me@yusufstar.com>
2025-05-08 23:04:13 +0300

    feat(ui): implement dashboard and login page with sidebar navigation and user authentication components

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-form"
+import Logo from "@/components/logo"
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <Logo svgclassname="w-6 h-6 text-primary" />
+
+      <LoginForm />
+    </div>
+  )
+}
---
+++ client/src/components/logo.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+export default function Logo({
+  size,
+  width = 24,
+  height = 24,
+  noIcon = false,
+  noText = false,
+  svgclassname = "",
+}: {
+  size?: number;
+  width?: number;
+  height?: number;
+  noText?: boolean;
+  noIcon?: boolean;
+  svgclassname?: string;
+}) {
+  return (
+    <Link href="/" className="flex items-center gap-2 self-center font-medium">
+      <div className="flex items-center gap-2">
+        {!noIcon && (
+          <svg
+            fill="none"
+            height={size || height}
+            viewBox="0 0 32 32"
+            width={size || width}
+            className={svgclassname}
+          >
+            <path
+              clipRule="evenodd"
+              d="M6 7h20v3H6V7zm0 8h9v10H6V15zm12 0h8v4h-8v-4zm0 6h8v4h-8v-4z"
+              fill="currentColor"
+              fillRule="evenodd"
+            />
+          </svg>
+        )}
+
+        {!noText && (
+          <span>B&M</span>
+        )}
+      </div>
+    </Link>
+  );
+}
